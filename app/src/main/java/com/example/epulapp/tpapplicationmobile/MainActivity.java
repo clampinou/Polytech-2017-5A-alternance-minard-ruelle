3f6ccccdc61ba198f3e2bec03f16cb1977c2d574
Epulapp <Epulapp@EPULAPP67.univ-lyon1.fr>
2017-11-28 17:25:35 +0100

    Ajout du multilingue

--- app/src/main/java/com/example/epulapp/tpapplicationmobile/MainActivity.java
+++ app/src/main/java/com/example/epulapp/tpapplicationmobile/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.epulapp.tpapplicationmobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("lifeCycle", "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("lifeCycle", "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("lifeCycle", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("lifeCycle", "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("lifeCycle", "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("lifeCycle", "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("lifeCycle", "onRestart");
     }
 }
