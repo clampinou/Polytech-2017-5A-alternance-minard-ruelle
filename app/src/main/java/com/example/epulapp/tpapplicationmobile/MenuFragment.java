27ea577ee4de04bea9a8851c8acf60cf0431fdb6
Epulapp <Epulapp@EPULAPP67.univ-lyon1.fr>
2017-11-29 11:13:25 +0100

    Ajout de fragments avec gestion click V1

--- app/src/main/java/com/example/epulapp/tpapplicationmobile/MenuFragment.java
+++ app/src/main/java/com/example/epulapp/tpapplicationmobile/MenuFragment.java
@@ -4,9 +4,11 @@ import android.app.Fragment;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -18,16 +20,16 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class MenuFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+//     TODO: Rename parameter arguments, choose names that match
+//     the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 //    private static final String ARG_PARAM1 = "param1";
 //    private static final String ARG_PARAM2 = "param2";
-//
-    // TODO: Rename and change types of parameters
+
+//     TODO: Rename and change types of parameters
 //    private String mParam1;
 //    private String mParam2;
 
-//    private OnFragmentInteractionListener mListener;
+    private OnFragmentInteractionListener mListener;
 
     public MenuFragment() {
         // Required empty public constructor
@@ -59,7 +61,21 @@ public class MenuFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_menu, container, false);
     }
-//
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        final Button oneVsIa = getActivity().findViewById(R.id.one_vs_ia);
+        oneVsIa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mListener.onFragmentInteraction(view.getId());
+            }
+        });
+    }
+
+    //
     // TODO: Rename method, update argument and hook method into UI event
 //    public void onButtonPressed(Uri uri) {
 //        if (mListener != null) {
@@ -67,22 +83,22 @@ public class MenuFragment extends Fragment {
 //        }
 //    }
 //
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-//
-//    @Override
-//    public void onDetach() {
-//        super.onDetach();
-//        mListener = null;
-//    }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
 //
 //    /**
 //     * This interface must be implemented by activities that contain this
@@ -94,8 +110,12 @@ public class MenuFragment extends Fragment {
 //     * "http://developer.android.com/training/basics/fragments/communicating.html"
 //     * >Communicating with Other Fragments</a> for more information.
 //     */
-//    public interface OnFragmentInteractionListener {
-//        // TODO: Update argument type and name
-//        void onFragmentInteraction(Uri uri);
-//    }
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(int id);
+    }
+
+
+
+
 }
